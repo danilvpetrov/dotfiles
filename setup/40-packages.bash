7a15bfb4fd850a254cf29600c8caa66c91878799
Danil Petrov <danilpetrovvi@gmail.com>
2020-03-31 20:35:29 +1000

    Add some resilience to pkg installation.

--- setup/40-packages.bash
+++ setup/40-packages.bash
@@ -2,9 +2,12 @@ set -e
 
 echo "Installing packages..."
 
+# install pygments.rb and travis without failing
 gem install --user-install \
   pygments.rb \
-  travis
+  travis || true
 
-brew bundle "--file=$HOME/Brewfile"
-brew link --force mysql-client
+# install whatever we can without failing.
+brew bundle "--file=$HOME/Brewfile" || true
+# link MySQL client without failing.
+brew link --force mysql-client || true
